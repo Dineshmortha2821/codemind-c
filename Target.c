d33bffa63f9c234dd9137458c6a6835548b0822a
Dineshmortha2821 <147487602+Dineshmortha2821@users.noreply.github.com>
2023-12-16 22:59:23 +0530

    #include<stdio.h> int main() {     int a1,a2,a3,a4;     scanf("%d%d%d%d",&a1,&a2,&a3,&a4);     if(a1>=10&&a2>=10&&a3>=10&&a4>=10){     printf("YES");     }     else{     printf("NO");     }
    }

---
+++ Target.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a1,a2,a3,a4;
+    scanf("%d%d%d%d",&a1,&a2,&a3,&a4);
+    if(a1>=10&&a2>=10&&a3>=10&&a4>=10){
+    printf("YES");
+    }
+    else{
+    printf("NO");
+    }
+    
+}
